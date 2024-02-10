07ce3fac0bc1020358cc9064b46627ee22ae27e0
dzjeri <2405godnadvore@gmail.com>
2024-02-10 07:16:16 +0300

    fixed port declaration

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/api/persons', (req, res) => {
     .then(savedPerson => res.json(savedPerson));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
