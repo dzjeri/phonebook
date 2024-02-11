2d0262c2e8a31ab88952445cf83053155190f98c
dzjeri <2405godnadvore@gmail.com>
2024-02-11 04:05:03 +0300

    updated delete method to work with Person model

--- index.js
+++ index.js
@@ -61,9 +61,9 @@ app.get('/api/persons/:id', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-  res.status(204).end();
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => res.status(204).end())
+    .catch(error => console.log(error));
 });
 
 const generateId = () => {
